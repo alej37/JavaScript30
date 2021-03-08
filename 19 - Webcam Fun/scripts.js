e7bd38b0f5b80f3f1630aac4bf75ddea613104d3
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2021-03-08 15:43:27 -0500

    SOLVED

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -25,7 +25,7 @@ function paintToCanvas() {
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
     let pixels = ctx.getImageData(0,0,width,height);
-    pixels = greenScreen(pixels)
+    npixels = greenScreen(pixels)
     ctx.putImageData(pixels, 0, 0)
   }, 16);
 }
